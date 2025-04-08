69fe98890f945e60dfa6e9eeec08789cbcb245be
Yldrabrettspill <mariuslo@uia.no>
2025-04-08 11:29:13 +0200

    Add files via upload

--- lykkekort.mjs
+++ lykkekort.mjs
@@ -1,7 +1,7 @@
 const LYKKE_KORT = [
     {id:1, bilde: "bilder/lykkekort.jpg", lyd:"lydfiler/Strand1,7,8,12.mp3",tekst:"Du får en hval!" },
     {id:2, bilde: "bilder/lykkekort.jpg", lyd:"lydfiler/Strand2.mp3",tekst:"Du får to hvaler, gi en til en annen spiller og ta en selv!" },
-    {id:3, bilde: "bilder/lykkekort.jpg", lyd:"lydfiler/3,11.mp3",tekst:"Haiene har tatt en av hvalene dine!" },
+    {id:3, bilde: "bilder/lykkekort.jpg", lyd:"lydfiler/Strand3,11.mp3",tekst:"Haiene har tatt en av hvalene dine!" },
     {id:4, bilde: "bilder/lykkekort.jpg", lyd:"lydfiler/Strand4.mp3",tekst:"Blekkspruten holder deg igjen, og han tar en hval av deg!" },
     {id:5, bilde: "bilder/lykkekort.jpg", lyd:"lydfiler/Strand5.mp3",tekst:"Blekkspruten tar en hval av deg, og han gir den til spilleren til høyre for deg!" },
     {id:6, bilde: "bilder/lykkekort.jpg", lyd:"lydfiler/Strand6,9,10.mp3",tekst:"Du har fått en livbøye, den kan du bruke til å redde en annen spiller fra havet!" },
@@ -10,7 +10,11 @@ const LYKKE_KORT = [
     {id:9, bilde: "bilder/lykkekort.jpg", lyd:"lydfiler/Strand6,9,10.mp3",tekst:"Du har fått en livbøye, den kan du bruke til å redde en annen spiller fra havet!" },
     {id:10, bilde: "bilder/lykkekort.jpg", lyd:"lydfiler/Strand6,9,10.mp3",tekst:"Du har fått en livbøye, den kan du bruke til å redde en annen spiller fra havet!" },
     {id:11, bilde: "bilder/lykkekort.jpg", lyd:"lydfiler/Strand3,11.mp3",tekst:"Haiene har tatt en av hvalene dine!" },
-    {id:12, bilde: "bilder/lykkekort.jpg", lyd:"lydfiler/1,7,8,12.mp3",tekst:"Du får en hval!" },
+    {id:12, bilde: "bilder/lykkekort.jpg", lyd:"lydfiler/Strand1,7,8,12.mp3",tekst:"Du får en hval!" },
+    {id:13, bilde: "bilder/lykkekort.jpg", lyd:"lydfiler/Strand13.mp3",tekst:"Du har fått en livbøye, den kan du bruke til å redde en annen spiller fra havet!" },
+    {id:14, bilde: "bilder/lykkekort.jpg", lyd:"lydfiler/Strand14.mp3",tekst:"Blekkspruten tar en hval av deg, hvem vil du gi den til?" },
+    {id:15, bilde: "bilder/lykkekort.jpg", lyd:"lydfiler/Strand15.mp3",tekst:"Den til venstre for deg får en hval!" },
+    {id:16, bilde: "bilder/lykkekort.jpg", lyd:"lydfiler/Strand16.mp3",tekst:"Du har fått en livbøye, den kan du bruke til å redde en annen spiller fra havet!" },
 ]
 
 
--- utfkort.mjs
+++ utfkort.mjs
@@ -1,16 +1,21 @@
 const utfkort = [
-    {id:"utfkort1", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort1.mp3",tekst:"Nå kommer det et jordskjelv, fort dere og hopp 3 ganger på gulvet for å passe dere for sprekkene!" },
+    {id:"utfkort1", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort1-ny.mp3",tekst:"Fort dere å hoppe på gulvet for å passe dere fra sprekkene. Hopp tre ganger så høyt dere klarer! Få en hval, og gi en hval til en annen spiller." },
     {id:"utfkort2", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort2.mp3",tekst:"Få en hval eller flytt på en av haiene?" },
     {id:"utfkort3", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort3.mp3",tekst:"Gi en hval til en venn!" },
     {id:"utfkort4", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort4.mp3",tekst:"Flytt på en av haiene eller på blekkspruten?" },
     {id:"utfkort5", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort5.mp3",tekst:"Få en hval eller flytt på blekkspruten?" },
-    {id:"utfkort6", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort6.mp3",tekst:"Hvem er høyest av av alle spillerne? Still opp på rekke å finn ut av hvem som er høyest?" },
-    {id:"utfkort7", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort7.mp3",tekst:"Haiene hopper etter dere, alle sammen må legge seg ned på magen og telle til tre!" },
+    {id:"utfkort6", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort6-ny.mp3",tekst:"Hvem er høyest av av alle spillerne? Still opp på rekke å finn ut av hvem som er høyest? Få en hval, og gi en hval til en annen spiller." },
+    {id:"utfkort7", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort7-ny.mp3",tekst:"Haiene hopper etter dere, alle sammen må legge seg ned på magen og telle til tre! Hvalen redder deg opp igjen, ta en hval." },
     {id:"utfkort8", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort8.mp3",tekst:"Ta stein, saks, papir med den til høyre for deg, den som vinner får flytte på en av haiene!" },
-    {id:"utfkort9", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort9.mp3",tekst:"Første man til å ta av seg sokkene, også ta de på igjen!" },
+    {id:"utfkort9", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort9-ny.mp3",tekst:"Første man til å ta av seg sokkene, også ta de på igjen! Gi en hval til en venn." },
     {id:"utfkort10", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort10.mp3",tekst:"Blekkspruten er redd for farger, den med mest farger på klærne kan flytte på blekkspruten!" },
-    {id:"utfkort11", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort11.mp3",tekst:"Jordskjelvene truer øya fortsatt, gå en runde rundt bordet for å unngå jordskjelvet!" },
-    {id:"utfkort12", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort12.mp3",tekst:"Haien lusker rundt beina deres, alle må ta opp beina en hel runde!" },
+    {id:"utfkort11", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort11-ny.mp3",tekst:"Jordskjelvene truer øya fortsatt, gå en runde rundt bordet for å unngå jordskjelvet! Få en hval, og gi en hval til en annen spiller." },
+    {id:"utfkort12", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort12-ny.mp3",tekst:"Haien lusker rundt beina deres, alle må ta opp beina en hel runde! Gi en hval til en venn." },
+    {id:"utfkort13", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort13.mp3",tekst:"Haien prøver å ta en hval av deg, gjem deg under bordet og tell til fem for å la haien gå forbi! Flytt haien 1 plass." },
+    {id:"utfkort14", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort14.mp3",tekst:"Blekkspruten tar en hval av deg, gå rundt bordet og gi den til en annen spiller!" },
+    {id:"utfkort15", bilde:"bilder/utfordringskort.jpg", lyd:"lydfiler/Utfkort15.mp3",tekst:"Det flyter en livbøye forbi, ta den og pass på den til noen trenger å reddes!" },
+    
+
 ]
 
 export default utfkort
